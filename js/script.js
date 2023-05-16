cc1605e921d96c070003adafaa77c4c44f18fc26
robertobenfini <roberto.benfini@gmail.com>
2023-05-16 15:00:47 +0200

    corretta-sintassi

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 // nome
-let nome= prompt ("Come ti chiami?")
+let nome= prompt ("Come ti chiami?");
 
 // cognome
-let cognome= prompt ("Qual è il tuo cognome?")
+let cognome= prompt ("Qual è il tuo cognome?");
 
 // colore preferito
-let colore= prompt ("Qual è il tuo colore preferito?")
+let colore= prompt ("Qual è il tuo colore preferito?");
 
 // variabile password
-let password = (nome + cognome + colore + 21)
+let password = (nome + cognome + colore + 21);
 
 // password
-document.getElementById ("password").innerHTML = (password)
+document.getElementById ("password").innerHTML = `${password}`;
